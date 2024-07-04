e0188af0401b8d38040821ca3a6ef087323eb964
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-07-05 01:49:07 +0300

    “commit”

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,7 +4,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     '''Calculates the sum of all elements in a mixed list.
     '''
     return float(sum(mxd_lst))
