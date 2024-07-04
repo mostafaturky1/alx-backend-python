a76b9a303fd187dbc55c79a929aeb3a2942b0139
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-07-05 01:46:36 +0300

    “commit”

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Task 0's module.
+'''
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+    '''Calculates the sum of all elements in a mixed list.
+    '''
+    return float(sum(mxd_lst))
