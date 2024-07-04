8b012f474cc271bcfede14d819b93341a1c27411
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-07-05 02:44:06 +0300

    “commit”

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -3,7 +3,6 @@
 '''
 from typing import Sequence, Any, Union
 
-
 # The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     '''Returns the first element of a sequence or None if the sequence is empty.
