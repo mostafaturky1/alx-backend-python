ef0f3a108b81cf7609dd7a4548535d7406bfccc0
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-07-05 02:14:04 +0300

    “commit”

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+'''Task 0's module.
+'''
+from typing import Iterable, Sequence, List, Tuple
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-'''Task 0's module.
+'''Task 9's module.
 '''
-from typing import Iterable, Sequence, List, Tuple
+from typing import Iterable, List, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''Computes the length of a list of sequences.
+    '''
     return [(i, len(i)) for i in lst]
