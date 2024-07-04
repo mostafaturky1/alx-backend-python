aa16adae296350698b2889615618819da38d7db8
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-07-05 00:29:54 +0300

    “commit”

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """
+    This function takes two float numbers as input and returns their sum.
+
+    Parameters:
+    a (float): The first number to be added.
+    b (float): The second number to be added.
+
+    Returns:
+    float: The sum of the input numbers.
+
+    Example:
+    >>> add(1.5, 2.7)
+    4.2
+    """
+    return a + b
