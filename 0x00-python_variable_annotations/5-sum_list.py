6e8ad67175ac04b610481b79ea69b579e853046b
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-07-05 01:32:55 +0300

    “commit”

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Task 0's module.
+'''
+
+
+def sum_list(input_list: list[float]) -> float:
+    '''Calculates the sum of all elements in a list.
+    '''
+    return sum(input_list)
