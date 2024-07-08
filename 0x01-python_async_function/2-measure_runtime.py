16f9282e53351eac74a8e06c4e5e134c8f129e62
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-07-09 01:07:27 +0300

    “commit”

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -8,7 +8,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     '''Computes the average runtime of wait_n.
     '''
     start_time = time.time()
