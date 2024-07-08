8860c1c706b52a81e5661984ec4aad47719f4d2b
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-07-09 00:37:29 +0300

    “commit”

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+'''Task 1's module.
+'''
+import asyncio
+import time
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measure the average time taken by 'wait_n' function to
+    complete 'n' coroutines,
+    each with a random delay between 0 and 'max_delay' seconds.
+
+    Parameters:
+    n (int): The number of coroutines to run.
+    max_delay (int): The maximum delay in seconds for each coroutine.
+
+    Returns:
+    float: The average time taken by 'wait_n' function to complete
+    all coroutines.
+    """
+    start_time = time.time()
+    await asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+
+    elapsed_time = end_time - start_time
+
+    return elapsed_time / n
