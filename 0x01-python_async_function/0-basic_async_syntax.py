c9d0d59053e1deb7f47c316fbed290bdd49a43ca
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-07-08 23:13:27 +0300

    “commit”

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: float = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """
     Asynchronously waits for a random amount of time between 0
     and `max_delay` seconds.
@@ -18,4 +18,4 @@ async def wait_random(max_delay: float = 10):
     """
     rendtime = random.uniform(0, max_delay)
     await asyncio.sleep(rendtime)
-    return float(rendtime)
+    return rendtime
