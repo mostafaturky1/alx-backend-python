999bd2138fb04a24313064a2b8bfd077ec59e9cd
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-07-10 01:51:51 +0300

    “commit”

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''Task 1's module.
+'''
+import asyncio
+import random
+from typing import List
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List:
+    '''An asynchronous comprehension that generates a list of
+    numbers from 0 to 9.
+    '''
+    return [num async for num in async_generator()]
