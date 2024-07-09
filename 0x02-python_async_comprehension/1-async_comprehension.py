d55489744355a6d9c2f20d2aa3df50315336153a
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-07-10 01:52:47 +0300

    “commit”

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -9,7 +9,7 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List:
+async def async_comprehension() -> List[float]:
     '''An asynchronous comprehension that generates a list of
     numbers from 0 to 9.
     '''
